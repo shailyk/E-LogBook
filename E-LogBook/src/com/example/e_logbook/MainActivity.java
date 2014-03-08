7370bfb37bd2c27072e72ba05163c5df6a69fb0d
shailyk <shailyk@hotmail.com>
2014-03-07 17:50:45 -0800

    Main Screen goes to Display message (need to rev)
    Need to reverse the parents

---
+++ E-LogBook/src/com/example/e_logbook/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.e_logbook;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+	
+	 public final static String EXTRA_MESSAGE = "com.example.e_logbook.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    public boolean openSettings(){
+        return true;
+ }   
+
+public boolean openSearch(){
+        return true;
+}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	 startActivity(intent);
+
+        // Do something in response to button
+    }
+    
+}
